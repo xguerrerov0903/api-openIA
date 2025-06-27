2dcd114a8ba020020d6c430ddca2107e750b81f9
xguerrerov0903 <xguerrerov0903@gmail.com>
2025-06-27 08:54:55 -0500

    Javascrip listo

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
-import OpenAI from "openai";
 
-async function sendPrompt() {
-    const userInput = document.getElementById("prom").value;
+const KEY = window.ENV.API_KEY
 
-    const client = new OpenAI();
+const prom = "Porque se usa JavaScrip?"
+async function sendPrompt(prom) {
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+        method: "POST",
+        headers: {
+            "Authorization": `Bearer ${KEY}`,
+            "Content-Type": "application/json"
+    },
+        body: JSON.stringify({
+        model: "gpt-4",
+        messages: [{ role: "user", content: prom }]
+    })
+});
+    
 
-    const completion = await client.chat.completions.create({
-        model: "gpt-4.1",
-        messages: [
-            {
-                role: "user",
-                content: userInput,
-            },
-        ],
-    });
-
-    console.log(completion.choices[0].message.content);
-
+const data = await response.json();
+console.log("Respuesta de OpenAI:");
+console.log(data.choices[0].message.content);
 }
+
+sendPrompt(prom);
+
+
+
+/*
+function handleClick() {
+    const userInput = document.getElementById("prom").value;
+    callOpenAI(userInput); // Aquí pasas el texto del input como prompt
+}
+    */
